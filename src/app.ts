01261569932c2f385dc8590ed5b9df1bfc4b7dd6
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-03-15 12:25:40 +0800

    chore: Set up the Node.js TypeScript Boilerplate

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+/**
+ * App Configuration
+ */
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({text: 'Hello World'});
+});
+
+/**
+ * Server activation
+ */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.info(`-- Server: run on http://localhost:${PORT} --`);
+});
